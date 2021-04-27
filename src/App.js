03822b0368290a8f9a87078f597cea8d7696bed1
Adham El Banhawy <the.benhawy@gmail.com>
2021-04-27 23:17:01 +0200

    added target date

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import {
   IonLabel,
   IonDatetime,
 } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 import BioCard from './components/BioCard'
 import { useLocalStorage } from './hooks'
 
 function App() {
   const [DOB, setDOB] = useLocalStorage('DOB', '')
-  const targetDate = new Date().toISOString();
+  const [targetDate, setTargetDate] = useState(new Date().toISOString());
   return (
     <IonApp>
       <IonHeader>
@@ -28,6 +28,13 @@ function App() {
           <IonDatetime displayFormat="DD-MMM-YYYY"
             value={DOB} 
             onIonChange={(e) => setDOB(e.detail.value)} 
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Target Date: </IonLabel>
+          <IonDatetime displayFormat="DD-MMM-YYYY"
+            value={targetDate} 
+            onIonChange={(e) => setTargetDate(e.detail.value)} 
           />
         </IonItem>
         {DOB && (<BioCard targetDate={targetDate} DOB={DOB}/>)}
